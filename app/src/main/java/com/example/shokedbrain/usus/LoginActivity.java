8c580ee8ffcfd378088d7c1acca976a5b50c0363
Andrew <a.solomencev@yandex.ru>
2017-04-16 23:42:44 +0300

    Working with backend

--- app/src/main/java/com/example/shokedbrain/usus/LoginActivity.java
+++ app/src/main/java/com/example/shokedbrain/usus/LoginActivity.java
@@ -1,14 +1,64 @@
 package com.example.shokedbrain.usus;
 
 import android.app.Activity;
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "MyActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance(); // Initialization instance
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null)
+                {
+                    Log.d(TAG,"onAuthStateChanged:signed_in");
+                }
+                else
+                {
+                    Log.d(TAG,"onAuthStateChanged:signed_out");
+                }
+            }
+        };
+
+        Button btn_log = (Button)findViewById(R.id.btn_login);
+        View.OnClickListener oclBtnLog = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast toast = Toast.makeText(getApplicationContext(),"DEV", Toast.LENGTH_SHORT);
+            }
+        };
+        btn_log.setOnClickListener(oclBtnLog);
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        if (mAuthListener != null)
+            mAuth.removeAuthStateListener(mAuthListener);
     }
 }
