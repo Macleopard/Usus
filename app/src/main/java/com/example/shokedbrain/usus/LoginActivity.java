ab338c05c0808599184aae2b71990df4b5282c1e
Andrew <a.solomencev@yandex.ru>
2017-04-20 20:03:14 +0300

    Сделал авторизацию (почти)

--- app/src/main/java/com/example/shokedbrain/usus/LoginActivity.java
+++ app/src/main/java/com/example/shokedbrain/usus/LoginActivity.java
@@ -9,12 +9,17 @@ import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
+import android.view.textservice.TextInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -22,25 +27,24 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private static final String TAG = "MyActivity";
-    private EditText email,pass;
+    private EditText email, pass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        email = (EditText)findViewById(R.id.email_input);
-        pass = (EditText)findViewById(R.id.pass_input);
+
+        email = (EditText) findViewById(R.id.email_input);
+        pass = (EditText) findViewById(R.id.pass_input);
         mAuth = FirebaseAuth.getInstance(); // Initialization instance
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null)
-                {
-                    Log.d(TAG,"onAuthStateChanged:signed_in");
-                }
-                else
-                {
-                    Log.d(TAG,"onAuthStateChanged:signed_out");
+                if (user != null) {
+                    Log.d(TAG, "onAuthStateChanged:signed_in");
+                } else {
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
                 }
             }
         };
@@ -54,8 +58,8 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 String inp = email.getText().toString();
-                if (TextUtils.isEmpty(inp)){
-                    email.setError(getString(R.string.emptyEmail_error));
+                if (!isValidEmail(inp)) {
+                    email.setError(getString(R.string.wrongFormat_email));
                 }
             }
 
@@ -73,7 +77,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 String inp = pass.getText().toString();
-                if (TextUtils.isEmpty(inp)){
+                if (TextUtils.isEmpty(inp) || inp.length() < 6) {
                     pass.setError(getString(R.string.emptyPass_error));
                 }
             }
@@ -83,29 +87,51 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
-        Button btn_log = (Button)findViewById(R.id.btn_login);
-        View.OnClickListener oclBtnLog = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast toast = Toast.makeText(getApplicationContext(),"DEV", Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        };
-        btn_log.setOnClickListener(oclBtnLog);
+        Button btn_log = (Button) findViewById(R.id.btn_login);
+        try {
+            View.OnClickListener oclBtnLog = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String log = email.getText().toString();
+                    String password = pass.getText().toString();
+                    mAuth.signInWithEmailAndPassword(log, password)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast toast = Toast.makeText(getApplicationContext(), "Logged!", Toast.LENGTH_SHORT);
+                                        toast.show();
+                                    } else {
+                                        Toast toast = Toast.makeText(getApplicationContext(), "No!", Toast.LENGTH_SHORT);
+                                        toast.show();
+                                    }
+                                }
+                            });
+
+                }
+            };
+            btn_log.setOnClickListener(oclBtnLog);
+        } catch (IllegalArgumentException e) {
+            Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.empty_error), Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
 
     @Override
-    public void onStart()
-    {
+    public void onStart() {
         super.onStart();
         mAuth.addAuthStateListener(mAuthListener);
     }
 
     @Override
-    public void onStop()
-    {
+    public void onStop() {
         super.onStop();
         if (mAuthListener != null)
             mAuth.removeAuthStateListener(mAuthListener);
+    }
+
+    public static boolean isValidEmail(String a) {
+        CharSequence target = a;
+        return !TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches();
     }
 }
