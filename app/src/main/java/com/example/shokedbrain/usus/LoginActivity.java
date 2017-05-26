71c4f772f7def319bdf2ac3972b48284ffa1a498
Andrew <a.solomencev@yandex.ru>
2017-05-26 12:51:22 +0300

    Добавил логотип, изменил иконку приложения

--- app/src/main/java/com/example/shokedbrain/usus/LoginActivity.java
+++ app/src/main/java/com/example/shokedbrain/usus/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.shokedbrain.usus;
 
 import android.content.Intent;
+import android.os.Process;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,7 +13,9 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+import android.app.ProgressDialog;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
